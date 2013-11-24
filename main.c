031c8891a0c8baf43811fb149bda9a188fb023bb
Tim Hsu <tim37021@gmail.com>
2013-11-25 02:45:55 +0800

    Try to light up LED but fail

--- main.c
+++ main.c
@@ -27,6 +27,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stm32f4xx_gpio.h"
 
 /** @addtogroup Template_Project
   * @{
@@ -49,6 +50,21 @@ static void Delay(__IO uint32_t nTime);
   * @param  None
   * @retval None
   */
+
+void GPIO_Output_Config(void){
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  GPIO_PinAFConfig(GPIOE, GPIO_PinSource3|GPIO_PinSource4|GPIO_PinSource5|GPIO_PinSource6|GPIO_PinSource7|GPIO_PinSource8|GPIO_PinSource9|GPIO_PinSource10|GPIO_PinSource11|GPIO_PinSource12|GPIO_PinSource13|GPIO_PinSource14, GPIO_AF_TIM3);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9|GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;            // Alt Function - Push Pull
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init( GPIOE, &GPIO_InitStructure );
+}
+
+
 int main(void)
 {
   /*!< At this stage the microcontroller clock setting is already configured, 
@@ -63,6 +79,8 @@ int main(void)
   RCC_GetClocksFreq(&RCC_Clocks);
   SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
 
+
+  GPIO_Output_Config();
   /* Add your application code here */
   /* Insert 50 ms delay */
   Delay(5);
@@ -70,6 +88,7 @@ int main(void)
   /* Infinite loop */
   while (1)
   {
+	GPIO_SetBits(GPIOE, GPIO_Pin_10);
   }
 }
 
